f93da0f2906c90853ee251279424cdb359c047b8
irevoire <irevoire@hotmail.fr>
2018-07-07 15:59:12 +0200

    indent

--- image.c
+++ image.c
@@ -14,7 +14,8 @@ image_t create_image(char *path, int x, int y, int w, int h)
 	if (image->surface == NULL)
 		handle_SDL_Error("Can't load BMP");
 
-	image->texture = SDL_CreateTextureFromSurface(window->renderer, image->surface);
+	image->texture = SDL_CreateTextureFromSurface(window->renderer,
+			image->surface);
 	if (image->texture == NULL)
 		handle_SDL_Error("Can't create surface");
 
